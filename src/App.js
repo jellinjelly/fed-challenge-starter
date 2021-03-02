38154162f25afd4b5ef5059e21b32efe343510db
Victoria Chen <vicchen12@gmail.com>
2021-03-02 12:17:27 -0800

    remove app.css file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Cards from "./Cards";
 import { Container } from "react-bootstrap";
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
